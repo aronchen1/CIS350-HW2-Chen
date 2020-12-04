b6ebe99f6c98535d943d050a60f11cea092381ab
aronchen1 <59976481+aronchen1@users.noreply.github.com>
2020-12-04 15:49:52 -0500

    finish

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -11,6 +11,14 @@ public class SimpleCalculator {
     public int div(int a, int b) {
         return a / b;
     }
+
+    public int mod(int a, int b){
+        return a % b;
+    }
+
+    public int squareRoot(int a){
+        return (int)Math.sqrt(a);
+    }
     public static final void main(String[] args) {
         System.out.println("Hey there CIS350.");
     }
--- tests/SimpleCalculatorTest.java
+++ tests/SimpleCalculatorTest.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
+import sun.java2d.pipe.SpanShapeRenderer;
+
 public class SimpleCalculatorTest {
     @Test
     public void testAdd() {
@@ -22,4 +24,19 @@ public class SimpleCalculatorTest {
         SimpleCalculator calc = new SimpleCalculator();
         assertEquals(calc.div(8, 2), 4);
     }
+
+    //modulo
+    @Test
+    public void testMod() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.mod(5,2),1);
+    }
+
+    @Test
+    public void testSqaureRoot(){
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.squareRoot(9),3);
+    }
+
+
 }
